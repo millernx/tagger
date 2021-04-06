92c99c35f61a14aa5aa4e3dddb618d59e18b65cc
Nicolas Miller <millernx@Nic.localdomain>
2021-04-05 18:39:10 -0600

    basic functionality done

--- content/urls.py
+++ content/urls.py
@@ -5,6 +5,7 @@ app_name = "content"
 urlpatterns = [
     path('', views.index, name="index"),
     path('experiment/<int:experiment_id>', views.experiment, name='experiment_detail'),
+    path('vote/<int:content_id>', views.vote, name='vote'),
     #path('content/<int:content_id>', views.content, name='content_detail'),
 ]
 
--- content/views.py
+++ content/views.py
@@ -1,25 +1,52 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-from .models import Experiment, Content
+from .models import Experiment, Content, Vote
+
+def get_user(u_id):
+    return User.objects.get(id=u_id)
 
 # Create your views here.
 
 def index(request):
     """Link to the different experiments"""
-    experiments = Experiment.objects.order_by('-id')[:5]
+    experiments = Experiment.objects.order_by('-id')
     context = {'experiments': experiments}
 
     return render(request, 'content/index.html', context)
 
 def experiment(request, experiment_id):
-    """List 5 contents from the experiment (eventually we can order by conflicting votes)"""
-    contents = Content.objects.filter(experiment__id=experiment_id).order_by('-id')[:10]
-    context = {'content_list': contents}
+    """
+    Specification:
+        Display a content the user has not voted on.
+        Display checkboxes with the posible labels for the experiment
+        Display a save button which registers a vote to the db and refreshes the page with a new content
+    """
+    user_voted_contents = [content['content_id'] for content in list(Vote.objects.filter(user_id=request.user.id).values('content_id').distinct('content_id'))]
+    contents = Content.objects.filter(experiment__id=experiment_id).exclude(id__in=user_voted_contents)
+    if len(contents)>0:
+        assigned_content = contents[0]
+    else:
+        return HttpResponse('No more contents') #TODO: render No more contents, link to homepage
+    context = {'content': assigned_content}
+    print (context['content'])
     return render(request, 'content/experiment.html', context)
 
-#def label(request):
-    #contents = 
-#def content(request, content_id):
-    #return HttpResponse("You're viewing content number %s" % content_id)
+def vote(request, content_id):
+    """
+    Check that the user hasn't voted on this content before
+    Create a vote for the content_id using vote value"""
+    user = get_user(request.user.id)
+    content = Content.objects.get(id=content_id)
+    if Vote.objects.filter(user_id=request.user.id).filter(content_id=content_id).exists():
+        return HttpResponse('you already voted') #redirect
+    else:
+        if request.POST['vote']=='yes':
+            v = Vote(user=user, content=content, value=True)
+        elif request.POST['vote']=='no':
+            v = Vote(user=user, content=content, value=False)
+        v.save()
+        return HttpResponseRedirect(reverse('content:experiment_detail', args=(content.experiment_id,)))
 
